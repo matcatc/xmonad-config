ae9654ae2d7264e2e8e016e22485231f55df387b
user <user@debian-virt-test>
2013-03-04 14:13:07 -0800

    Fixed trayer colors
    Added note regarding getting trayer to display the correct color.

--- start_trayer.sh
+++ start_trayer.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 #
 # Simply starts trayer for us with all our desired options
+#
+# When running in the xmonad testing vm, using --transparent false (default)
+# prevented --tint from working. But setting transparent to true did. So to
+# ensure that the proper color was being displayed, set transparent to true,
+# but make alpha 0 so that only the tint color shows through.
 
-trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --height 19 --transparent true --tint 0x000066 &
+
+trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --height 19 --transparent true --alpha 0 --tint 0x285577 &
 
