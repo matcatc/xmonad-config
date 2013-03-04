58af5956dc51e4bfd1a805a86bbc30e36f469abe
user <user@debian-virt-test>
2013-03-04 14:54:27 -0800

    Trying out different fonts for xmobar

--- start_trayer.sh
+++ start_trayer.sh
@@ -6,7 +6,10 @@
 # prevented --tint from working. But setting transparent to true did. So to
 # ensure that the proper color was being displayed, set transparent to true,
 # but make alpha 0 so that only the tint color shows through.
+#
+# The height is determined to match xmobar's height. And xmobar's height is
+# based on the font used. So we'll need to change the height whenever we change
+# the font being used.
 
-
-trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --height 19 --transparent true --alpha 0 --tint 0x285577 &
+trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --heighttype pixel --height 17 --transparent true --alpha 0 --tint 0x285577 &
 
