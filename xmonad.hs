ebbe3639ce0342f24fb82ccff8a5ff073994e914
user <user@debian-virt-test>
2013-03-12 21:25:31 -0700

    Reordered and refactored entire file
    Just making it cleaner and easier to work with as a whole. No functionality
    changes.

--- xmonad.hs
+++ xmonad.hs
@@ -3,16 +3,17 @@ import XMonad
 -- for xmobar
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import System.IO (hPutStrLn)
 import XMonad.Util.Run (spawnPipe)
 
-import qualified Data.Map as M
+-- for various things
 import Data.List (sort)
-import System.IO (hPutStrLn)
+import qualified Data.Map as M
+import qualified XMonad.StackSet as W
 
 -- for dynamic workspaces
-import qualified XMonad.StackSet as W
-import XMonad.Actions.DynamicWorkspaces
 import XMonad.Actions.CopyWindow (copy)
+import XMonad.Actions.DynamicWorkspaces
 
 -- for prompts (used by various different features)
 import XMonad.Prompt
@@ -38,25 +39,32 @@ import XMonad.Hooks.ManageHelpers
 import Graphics.X11.ExtraTypes.XF86	-- special key sybmols
 
 
----------------------------------------------------
+
+
+
+
+------------------------------------------------------------------------------
 -- General TODOs
----------------------------------------------------
+------------------------------------------------------------------------------
 --  test out multihead support
 --  test out different layouts
 --  color scheme (similar to my current wmii color scheme)
---  new windows inserted below focused window (this is in the faq, not sure about which soln to choose though)
 --
 --  refactor
---  reorder code blocks (main currently stuck in the middle)
 --  extract constants
 --
 --  make a custom keybindings file (using the svg provided for the defaults)
 
 
 
----------------------------------------------------
+
+
+
+
+
+------------------------------------------------------------------------------
 -- Constants
----------------------------------------------------
+------------------------------------------------------------------------------
 
 --
 --- color definitions
@@ -94,16 +102,26 @@ myFocusedBorderColor 	= bronze
 
 
 
----------------------------------------------------
+
+
+
+
+
+
+------------------------------------------------------------------------------
 -- layouts
----------------------------------------------------
--- avoidStruts is for enabling docks (xmobar, dmenu)
--- TODO: IM layout?
+------------------------------------------------------------------------------
+-- avoidStruts is for enabling docks (xmobar, trayer, etc.)
 myLayouts = onWorkspace "im" im $
-	    Mirror tall ||| tall ||| StackTile 1 (3/100) (1/2) ||| Full ||| simpleTabbed
+	    Mirror tall ||| tall ||| stack ||| Full ||| simpleTabbed		-- defaults
 	where
-		tall = Tall 1 (3/100) (1/2)
-		im = gridIM (1%7) (ClassName "Pidgin")
+		nmaster = 1
+		delta 	= 3/100
+		ratio 	= 1/2
+
+		tall 	= Tall nmaster delta ratio
+		stack 	= StackTile nmaster delta ratio
+		im 	= gridIM (1%7) (ClassName "Pidgin")			-- TODO: test
 
 
 -- apply layout modifiers
@@ -113,55 +131,64 @@ myLayoutHook = avoidStruts $ myLayouts
 
 
 
----------------------------------------------------
+
+
+
+------------------------------------------------------------------------------
 -- workspaces
----------------------------------------------------
+------------------------------------------------------------------------------
 myWorkspaces = map show [0..9] ++ sort ["mail", "music", "upgrade", "im", "backup"]
 
 
 -- manage particular windows
 --  send them to particular workspaces or make the float, etc.
-myManageHook = composeOne					-- applies the first one that matches
-	    [ checkDock              	-?> doIgnore 		-- equivalent to manageDocks
+myManageHook = composeOne						-- applies the first one that matches
+		[ checkDock              	-?> doIgnore 		-- equivalent to manageDocks
 
-	-- floating
-	    , isDialog               	-?> doFloat
-	    , className =? "Xmessage" 	-?> doFloat
-	    , className =? "Gimp"    	-?> doFloat
-	    , className =? "MPlayer" 	-?> doFloat
+		-- floating
+		, isDialog               	-?> doFloat
+		, className =? "Xmessage" 	-?> doFloat
+		, className =? "Gimp"    	-?> doFloat
+		, className =? "MPlayer" 	-?> doFloat
 
-	-- shifting
-	    , className =? "Pidgin" 	-?> doShift "im"
+		-- shifting
+		, className =? "Pidgin" 	-?> doShift "im"
 
-	-- default
-	    , return True 		-?> doF W.swapDown	-- new windows appear one down
-	    ]
+		-- complex
+
+		-- default
+		, return True 			-?> doF W.swapDown	-- new windows appear one down
+		]
 
 
 
----------------------------------------------------
+
+
+
+
+
+
+------------------------------------------------------------------------------
 -- Log Hook related stuff
----------------------------------------------------
+------------------------------------------------------------------------------
 --
 -- This allows us to have multiple dynamicLogWithPP's. The main reason to do
 -- this is so we can have multiple xmobars (e.g: one up top and one at the
 -- bottom)
 --
 -- Note: I have no idea whether this is broken/buggy in some subtle way, as my
--- Haskell (and particularly xmonad) experience is limited. In particular, I'm
--- worried about the return / effect for X (). That being said, its currently
--- working, so it can't be that bad.
+-- Haskell (and particularly xmonad) experience is limited. That being said,
+-- its currently working, so it can't be that bad.
 --
 -- My understanding is X () means that it effects the X monad but has no
 -- return. Ie: something on the screen changes.
 multiDynamicLogWithPP :: [PP] -> X ()
 -- No PP, so don't do anything
 multiDynamicLogWithPP [] = return ()
-multiDynamicLogWithPP [pp] = do
-	dynamicLogWithPP pp
-multiDynamicLogWithPP (pp:pps) = do
-	dynamicLogWithPP pp
-	multiDynamicLogWithPP pps
+multiDynamicLogWithPP [pp] = dynamicLogWithPP pp
+multiDynamicLogWithPP (pp:pps) = dynamicLogWithPP pp >> multiDynamicLogWithPP pps
+
+
 
 -- Below are my PP's for my top and bottom xmobars. Since I run xmobar via a
 -- proc, and the procs are started in main and thus only in scope there, we
@@ -188,7 +215,15 @@ xmobarBottomPP proc = xmobarPP
 
 
 
----------------------------------------------------
+
+
+
+
+
+
+
+
+------------------------------------------------------------------------------
 -- startup programs
 -- see: .xsession and .Xinitrc
 --
@@ -196,14 +231,54 @@ xmobarBottomPP proc = xmobarPP
 -- being), b/c then whenever I reloaded xmonad (e.g: when testing out a
 -- configuration change), it'd end up also starting up those programs. Which means
 -- I'd end up with extra running copies, which I don't want.
----------------------------------------------------
+------------------------------------------------------------------------------
 
 
 
 
----------------------------------------------------
+
+
+
+
+
+------------------------------------------------------------------------------
+-- XMonad.Prompt configuration
+------------------------------------------------------------------------------
+--
+-- I think alwaysHighlight will cause the prompts to be like in wmii (e.g:
+-- dmenu, tab switching) where the first autocompletion is automatically
+-- highlighted. So that simply pressing enter then results in this autocompletion
+-- to be selected. Currently, I have to press an extra tab to run a program.
+--
+-- Note: it appears this feature is in xmonad 0.11 but not 0.10. I'm currently
+-- running 0.10 (debian testing's package), and it fails to compile when I try
+-- to use it. Error given is: "'alwaysHighlight' is not a (visible) constructor
+-- field name"
+--
+-- Note: I don't like the autocompletion feature b/c it automatically selects
+-- it and acts as if we press enter. A typo could easily lead to running god knows
+-- what.
+--
+-- TODO: dimensions (to match xmobar)
+myXPConfig = defaultXPConfig
+	{
+	  bgColor = wmiiBlue -- darkBlue
+	, position = Bottom		-- TODO: bottom vs top?
+--	, alwaysHighlight = True
+	}
+
+
+
+
+
+
+
+
+
+
+------------------------------------------------------------------------------
 -- Action prompt
----------------------------------------------------
+------------------------------------------------------------------------------
 --
 -- like wmii's action list, which had special commands to give the computer
 -- much like running normal programs. E.g: shutdown, etc.
@@ -246,60 +321,16 @@ actionPrompt config = inputPromptWithCompl config "Action" (mkComplFunFromList a
 
 
 
----------------------------------------------------
--- main
----------------------------------------------------
---
--- Starts up all the necessary processes
--- Then starts up xmonad with our modified version of the defaultConfig
---
-main = do
-	-- make sure that there's spaces in the concating of the string, so as
-	--  to prevent program args from running together
-	xmobarTopProc <- spawnPipe $ "xmobar"
-			++ " --bgcolor=" ++ systemFGColor
-			++ " --fgcolor=" ++ lightGrey
-			++ " ~/.xmonad/xmobar_top_rc"
-	xmobarBottomProc <- spawnPipe $ "xmobar"
-			++ " --bgcolor=" ++ systemFGColor
-			++ " --fgcolor=" ++ lightGrey
-			++ " ~/.xmonad/xmobar_bottom_rc"
-	xmonad $ defaultConfig
-		{
-                  terminal    	= myTerminal
-		, modMask     	= myModMask
-		, keys 	      	= myKeys <+> keys defaultConfig
-		, mouseBindings = myMouseBindings -- <+> mouseBindings defaultConfig
-
-		--
-		--- aesthetics
-		--
-		, borderWidth 	     = myBorderWidth
-		, normalBorderColor  = myNormalBorderColor
-		, focusedBorderColor = myFocusedBorderColor
-
-		--
-		--- layouts and window management
-		--
-		, manageHook = myManageHook <+> manageDocks <+> manageHook defaultConfig
-		, layoutHook = myLayoutHook 
-		, workspaces = myWorkspaces
-
-		--
-		---  xmobars
-		--   see above for def of xmobarXPP's
-		--
-		, logHook = multiDynamicLogWithPP
-				[ xmobarTopPP xmobarTopProc
-				, xmobarBottomPP xmobarBottomProc
-				]
-		}
 
 
 
----------------------------------------------------
+
+
+
+
+------------------------------------------------------------------------------
 -- custom keybindings
----------------------------------------------------
+------------------------------------------------------------------------------
 --  TODO: clean up
 --
 myKeys conf @(XConfig {XMonad.modMask = myModMask}) = M.fromList $
@@ -311,6 +342,8 @@ myKeys conf @(XConfig {XMonad.modMask = myModMask}) = M.fromList $
 	++
 
 	-- dynamic workspaces
+	-- makes workspaces dynamic like in wmii and others, where they're
+	--  created on use and deleted when no longer needed.
 	[
 	  ((myModMask .|. shiftMask, xK_BackSpace), removeEmptyWorkspace)							-- remove current workspace
 	, ((myModMask              , xK_t        ), removeEmptyWorkspaceAfterExcept myWorkspaces (selectWorkspace myXPConfig))	-- select workspace
@@ -359,6 +392,8 @@ myKeys conf @(XConfig {XMonad.modMask = myModMask}) = M.fromList $
 	, ((0, xF86XK_AudioNext       ), spawn "xmessage -default okay 'TODO: implement next'")
 	, ((0, xF86XK_AudioPrev       ), spawn "xmessage -default okay 'TODO: implement prev'")
 	]
+	++
+
 
 	-- TODO: other speical keybindings?
 
@@ -371,11 +406,24 @@ myKeys conf @(XConfig {XMonad.modMask = myModMask}) = M.fromList $
 --	++
 
 
+	-- empty list at the end makes it easier to comment out keybinding
+	-- blocks above. Because this way all blocks above have the (++) after
+	-- them, and then commenting out the last block doesn't cause us to
+	-- have to comment out a different block's (++)
+	[]
 
 
----------------------------------------------------
+
+
+
+
+
+
+
+
+------------------------------------------------------------------------------
 -- Mouse bindings
----------------------------------------------------
+------------------------------------------------------------------------------
 myMouseBindings :: XConfig Layout -> M.Map (KeyMask, Button) (Window -> X ())
 myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList
 	[ ((modMask, 		     button1), \w -> focus w >> mouseMoveWindow w >> windows W.shiftMaster)	-- float and move/drag
@@ -385,30 +433,62 @@ myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList
 	]
 
 
----------------------------------------------------
--- XMonad.Prompt configuration
----------------------------------------------------
+
+
+
+
+
+
+
+------------------------------------------------------------------------------
+-- main
+------------------------------------------------------------------------------
 --
--- I think alwaysHighlight will cause the prompts to be like in wmii (e.g:
--- dmenu, tab switching) where the first autocompletion is automatically
--- highlighted. So that simply pressing enter then results in this autocompletion
--- to be selected. Currently, I have to press an extra tab to run a program.
+-- Starts up all the necessary processes
+-- Then starts up xmonad with our modified version of the defaultConfig
 --
--- Note: it appears this feature is in xmonad 0.11 but not 0.10. I'm currently
--- running 0.10 (debian testing's package), and it fails to compile when I try
--- to use it. Error given is: "'alwaysHighlight' is not a (visible) constructor
--- field name"
---
--- Note: I don't like the autocompletion feature b/c it automatically selects
--- it and acts as if we press enter. A typo could easily lead to running god knows
--- what.
---
--- TODO: dimensions (to match xmobar)
-myXPConfig = defaultXPConfig
-	{
-	  bgColor = wmiiBlue -- darkBlue
-	, position = Bottom		-- TODO: bottom vs top?
---	, alwaysHighlight = True
-	}
+main = do
+	-- make sure that there's spaces in the concating of the string, so as
+	--  to prevent program args from running together
+	xmobarTopProc <- spawnPipe $ "xmobar"
+			++ " --bgcolor=" ++ systemFGColor
+			++ " --fgcolor=" ++ lightGrey
+			++ " ~/.xmonad/xmobar_top_rc"
+	xmobarBottomProc <- spawnPipe $ "xmobar"
+			++ " --bgcolor=" ++ systemFGColor
+			++ " --fgcolor=" ++ lightGrey
+			++ " ~/.xmonad/xmobar_bottom_rc"
+
+	xmonad $ defaultConfig
+		{
+                  terminal    	= myTerminal
+		, modMask     	= myModMask
+		, keys 	      	= myKeys <+> keys defaultConfig
+		, mouseBindings = myMouseBindings
+
+		--
+		--- aesthetics
+		--
+		, borderWidth 	     = myBorderWidth
+		, normalBorderColor  = myNormalBorderColor
+		, focusedBorderColor = myFocusedBorderColor
+
+		--
+		--- layouts and window management
+		--
+		, manageHook = myManageHook <+> manageDocks <+> manageHook defaultConfig
+		, layoutHook = myLayoutHook
+		, workspaces = myWorkspaces
+
+		--
+		---  xmobars
+		--   see above for def of xmobarXPP's
+		--
+		, logHook = multiDynamicLogWithPP
+				[ xmobarTopPP xmobarTopProc
+				, xmobarBottomPP xmobarBottomProc
+				]
+		}
+
 
 
