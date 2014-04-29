5b37ed75b7bb36e3c017019dc5eecbc48ac27d6d
Matthew Todd <matcatprg@yahoo.com>
2014-04-28 19:45:38 -0700

    Change Music Player over to Clementine
    Switching music multiplayer keys over to control Clementine Music Player

--- xmonad.hs
+++ xmonad.hs
@@ -507,10 +507,16 @@ myKeys conf @(XConfig {XMonad.modMask = myModMask}) = M.fromList $
 	  ((0, xF86XK_AudioRaiseVolume), spawn "~/bin/raise_volume.sh")
 	, ((0, xF86XK_AudioLowerVolume), spawn "~/bin/lower_volume.sh")
 	, ((0, xF86XK_AudioMute       ), spawn "~/bin/mute_volume.sh")
-	, ((0, xF86XK_AudioPlay       ), spawn "mpc toggle")
-	, ((0, xF86XK_AudioStop       ), spawn "mpc stop")
-	, ((0, xF86XK_AudioNext       ), spawn "mpc next")
-	, ((0, xF86XK_AudioPrev       ), spawn "mpc prev")
+    -- MPD setup
+--	, ((0, xF86XK_AudioPlay       ), spawn "mpc toggle")
+--	, ((0, xF86XK_AudioStop       ), spawn "mpc stop")
+--	, ((0, xF86XK_AudioNext       ), spawn "mpc next")
+--	, ((0, xF86XK_AudioPrev       ), spawn "mpc prev")
+  	-- Clementine setup
+	, ((0, xF86XK_AudioPlay       ), spawn "/usr/bin/clementine --play-pause")
+	, ((0, xF86XK_AudioStop       ), spawn "/usr/bin/clementine --stop")
+	, ((0, xF86XK_AudioNext       ), spawn "/usr/bin/clementine --next")
+	, ((0, xF86XK_AudioPrev       ), spawn "/usr/bin/clementine --previous")
 	]
 	++
 
